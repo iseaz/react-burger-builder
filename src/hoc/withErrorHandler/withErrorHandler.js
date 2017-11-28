82474ba2ba27967de215eb263958017b955bc1aa
Apisit Asavatappitak <apisit@ADAPTERDIGITAL.LOCAL>
2017-11-28 18:44:27 +0700

    added withErrorHandler

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Modal from '../../components/UI/Modal/Modal'
+import Aux from '../Auxi/Auxi'
+
+const withErrorHandler = WrappedComponent => {
+	return props => {
+		return (
+			<Aux>
+				<Modal>
+					Something didn't work!
+				</Modal>
+
+				<WrappedComponent {...props} />
+			</Aux>
+		)
+	}
+}
+
+export default withErrorHandler
